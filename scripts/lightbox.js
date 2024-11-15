d60a74d6cde84f78d2ac780c2ff8339c120979df
HolliJolli <hollygoodwill0@gmail.com>
2024-11-15 11:22:35 -0500

    wesdvnjkadsd fdvk.esbd
    Co-authored-by: MazeCow <MazeCow@users.noreply.github.com>

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -17,3 +17,23 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var modal = document.getElementById("calc-modal");
+  var moreInfoBtn = document.getElementById("calc-btn");
+  var closeBtn = document.getElementsByClassName("closes-btn")[0];
+
+  moreInfoBtn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+});
